8bcb835087db1d9ee155b6062e0ccbdd489df239
GlebLarkin <145963066+GlebLarkin@users.noreply.github.com>
2023-11-14 19:53:11 +0300

    Update waveMeasure.py
    по

--- Лаба 4/waveMeasure.py
+++ Лаба 4/waveMeasure.py
@@ -9,10 +9,11 @@ troyka = 13
 GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(comp, GPIO.IN)
+GPIO.setup(21, GPIO.IN)
+listADC = []
 
 def binary(n):
     return [int(i) for i in bin(n)[2:].zfill(8)]
-
 def adc():
     value = 0
     for i in range(7, -1, -1):
@@ -23,23 +24,19 @@ def adc():
             value -= 2**i
     return value
 
-print(2)
-GPIO.setup(21, GPIO.IN)
-listADC = []
+while 1:
+    print(GPIO.input(21))
+    if GPIO.input(21) == 1:
+        timeStart = time.time()
+        break
+
+listADC.append(adc()/256*3.3)
 
 while 1:
-    time.sleep(0.1)
-    print(GPIO.input(21))
-one = adc()/256*3.3
-listADC.append(one)
-vrem = int(input())
-timeStart = time.time()
-print(2)
-while 1:
-    one = adc()/256*3.3
-    listADC.append(one)
+    listADC.append(adc()/256*3.3)
     print(adc()/256*3.3)
-    if listADC[-1] != listADC[-2]:
+    if listADC[-1]  - listADC[-2] >= 0.5:
         delta = time.time() - timeStart
         break
 print(delta)
+
