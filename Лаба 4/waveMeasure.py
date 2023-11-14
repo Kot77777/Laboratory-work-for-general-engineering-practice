0bc222069808c9448b124de55f64ef9d3a529010
GlebLarkin <145963066+GlebLarkin@users.noreply.github.com>
2023-11-14 22:06:04 +0300

    Update waveMeasure.py

--- Лаба 4/waveMeasure.py
+++ Лаба 4/waveMeasure.py
@@ -2,7 +2,7 @@ import numpy as np
 import waveFunctions as b
 import RPi.GPIO as GPIO
 import time
-def k_finder(v,v0 , h):#эта функция определяет коэф k, определяющий зависимость напряжения от глубины v = v0 + k*h
+def k_finder(v,v0 , h):  # эта функция определяет коэф k, определяющий зависимость напряжения от глубины v = v0 + k*h
     #ожидаемо, к < 0
     return (v - v0)  / h
 def binary(n):  # перевод в двоичную сс
